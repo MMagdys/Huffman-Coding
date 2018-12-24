99ff166ef24a410381eb91927b7d230020758449
Muhammad <muhammad.magdy50@gmail.com>
2018-12-24 19:40:57 +0200

    [+] Directory Compression

--- HuffmanCompressor.py
+++ HuffmanCompressor.py
@@ -1,6 +1,6 @@
 from HuffmanTree import HuffmanTree
-import json
-import time
+import HuffmanHeaders
+import time, os
 
 
 class HuffmanCompressor(object):
@@ -9,6 +9,7 @@ class HuffmanCompressor(object):
 
 		self.encoded_txt = ""
 		self.encoded_tree = bytearray()
+		self.compression_bytes = bytearray()
 
 
 
@@ -20,7 +21,7 @@ class HuffmanCompressor(object):
 
 
 
-	def encoded_arr(self, encoded_txt, encoded_tree):
+	def encoded_arr(self, encoded_txt, encoded_tree, filename):
 
 		trail = 8 - len(encoded_txt) % 8
 		if trail:
@@ -32,10 +33,14 @@ class HuffmanCompressor(object):
 		comp_arr.append(trail)
 
 		# 2->5 bytes : length of encode tree
+		# 5 -> n : encode tree
 		tree_arr = len(encoded_tree).to_bytes(4, byteorder="little")
 		comp_arr.extend(tree_arr)
 		comp_arr.extend(self.encoded_tree)
 
+		# n-> m : file name and file extention
+
+		comp_arr.extend(HuffmanHeaders.file_header(filename))
 
 		data_arr = bytearray()
 		data_arr.extend(int(encoded_txt,2).to_bytes((len(encoded_txt))//8, byteorder="little"))
@@ -46,7 +51,7 @@ class HuffmanCompressor(object):
 
 
 
-	def compress(self, filename):
+	def compress_file_old(self, filename):
 
 		t1 = time.time()
 
@@ -56,7 +61,7 @@ class HuffmanCompressor(object):
 
 			tree = HuffmanTree()
 			codes , self.encoded_tree = tree.huffman_coding(txt)
-			print(codes)
+			# print(codes)
 			self.get_encoded_txt(txt, codes)
 
 		src.close()
@@ -69,4 +74,73 @@ class HuffmanCompressor(object):
 		t2 = time.time()
 
 		return t2 - t1, filename.split('.')[0]+".huffman"
+		# return self.encoded_arr(self.encoded_txt, self.encoded_tree)
 
+
+
+	def compress_file(self, filename):
+
+		
+		with open(filename, "rb") as src:
+
+			txt = src.read()
+
+			tree = HuffmanTree()
+			codes , self.encoded_tree = tree.huffman_coding(txt)
+			# print(codes)
+			self.get_encoded_txt(txt, codes)
+
+		src.close()
+
+		t2 = time.time()
+
+		return self.encoded_arr(self.encoded_txt, self.encoded_tree, filename)
+
+
+
+	def compress_files(self, filename, path="."):
+
+		if filename is None : filename = path
+		# if comp_arr is None:
+		# 	comp_arr = bytearray()
+
+		filename = path + "/" + filename
+		
+		if (os.path.isdir(filename)) :
+
+			dir_array = bytearray()
+
+			dir_header, files = HuffmanHeaders.dir_header(filename)
+			dir_array.extend(dir_header)
+
+			self.compression_bytes.extend(dir_array)
+
+			for file in files :
+				self.compress_files(file, filename)
+
+		else:
+
+			self.compression_bytes.extend(self.compress_file(filename))
+
+
+	def compress(self, filename):
+
+		t1 = time.time()
+
+		self.compress_files(filename)
+
+		# output, ext = HuffmanHeaders.filename_split(filename)
+
+		with open(filename.split(".")[0] + ".huffman", "wb") as dest:
+			dest.write(self.compression_bytes)
+
+		dest.close()
+
+		t2 = time.time()
+
+
+
+h = HuffmanCompressor()
+h.compress("folder1")
+# print(h.compression_bytes)
+# h.compress_file("folder1/f2")
